70886ddcd2bf6da44ed96b53a765fb236380efc2
Murugan Pandian <murugan@telecmi.com>
2023-03-15 10:12:46 +0530

    Package Update
    npm package update

--- src/esl/Server.ts
+++ src/esl/Server.ts
@@ -6,16 +6,14 @@ import { ICallback, IDictionary } from '../utils';
 
 export type IServerReadyCallback = ICallback<void>;
 
-export interface IServerOptions
-{
+export interface IServerOptions {
     myevents?: boolean;
     server?: net.Server;
     port?: number;
     host?: string;
 }
 
-export enum ServerEvent
-{
+export enum ServerEvent {
     ConnectionOpen = 'connection::open',
     ConnectionReady = 'connection::ready',
     ConnectionClose = 'connection::close',
@@ -25,60 +23,55 @@ export enum ServerEvent
 /**
  * A helper class for running a server that accepts multiple connections.
  */
-export class Server extends EventEmitter2
-{
+export class Server extends EventEmitter2 {
     readonly connections: IDictionary<Connection> = {};
     readonly server: net.Server;
 
     private _bindEvents: boolean;
 
-    constructor(readyCb?: IServerReadyCallback);
-    constructor(options: IServerOptions, readyCb?: IServerReadyCallback);
-    constructor(opts?: IServerOptions | IServerReadyCallback, readyCb?: IServerReadyCallback)
-    {
-        super({
+    constructor( readyCb?: IServerReadyCallback );
+    constructor( options: IServerOptions, readyCb?: IServerReadyCallback );
+    constructor( opts?: IServerOptions | IServerReadyCallback, readyCb?: IServerReadyCallback ) {
+        super( {
             wildcard: true,
             delimiter: '::',
             maxListeners: 25,
-        });
+        } );
 
-        if (typeof opts === 'function')
-        {
+        if ( typeof opts === 'function' ) {
             readyCb = opts as IServerReadyCallback;
             opts = undefined;
         }
 
-        if (readyCb)
-        {
-            this.once(ServerEvent.Ready, readyCb);
+        if ( readyCb ) {
+            this.once( ServerEvent.Ready, readyCb );
         }
 
-        this._bindEvents = opts && opts.myevents ? opts.myevents : false;
+        // this._bindEvents = opts && opts.myevents ? opts.myevents : false;
 
-        if (opts && opts.server)
-        {
-            this.server = opts.server;
+        // if (opts && opts.server)
+        // {
+        //     this.server = opts.server;
 
-            // make sure we dont call the callback before the function returns
-            process.nextTick(() => this.emit(ServerEvent.Ready));
+        //     // make sure we dont call the callback before the function returns
+        //     process.nextTick(() => this.emit(ServerEvent.Ready));
 
-            this.server.on('connection', this._onConnection.bind(this));
-        }
-        else
-        {
-            const port = opts && opts.port ? opts.port : 8022;
-            const host = opts && opts.host ? opts.host : '127.0.0.1';
+        //     this.server.on('connection', this._onConnection.bind(this));
+        // }
+        // else
+        // {
+        //     const port = opts && opts.port ? opts.port : 8022;
+        //     const host = opts && opts.host ? opts.host : '127.0.0.1';
 
-            this.server = net.createServer(this._onConnection.bind(this));
-            this.server.listen(port, host, () => this.emit(ServerEvent.Ready));
-        }
+        //     this.server = net.createServer(this._onConnection.bind(this));
+        //     this.server.listen(port, host, () => this.emit(ServerEvent.Ready));
+        // }
     }
 
     /**
      * Returns true if constructed with `myevents`.
      */
-    bindEventsEnabled(): boolean
-    {
+    bindEventsEnabled (): boolean {
         return this._bindEvents;
     }
 
@@ -87,39 +80,32 @@ export class Server extends EventEmitter2
      *
      * @param callback Called when the server is closed.
      */
-    close(callback?: (err?: Error | undefined) => void): void
-    {
-        this.server.close(callback);
+    close ( callback?: ( err?: Error | undefined ) => void ): void {
+        this.server.close( callback );
     }
 
-    private _onConnection(socket: net.Socket): void
-    {
-        const conn = new Connection(socket, ConnectionType.Outbound);
+    private _onConnection ( socket: net.Socket ): void {
+        const conn = new Connection( socket, ConnectionType.Outbound );
         const id = uuid.v4();
 
         this.connections[id] = conn;
 
-        this.emit(ServerEvent.ConnectionOpen, conn, id);
+        this.emit( ServerEvent.ConnectionOpen, conn, id );
 
-        conn.on(ConnectionEvent.Ready, () =>
-        {
-            if (this._bindEvents)
-            {
-                conn.sendRecv('myevents', () =>
-                {
-                    this.emit(ServerEvent.ConnectionReady, conn, id);
-                });
+        conn.on( ConnectionEvent.Ready, () => {
+            if ( this._bindEvents ) {
+                conn.sendRecv( 'myevents', () => {
+                    this.emit( ServerEvent.ConnectionReady, conn, id );
+                } );
             }
-            else
-            {
-                this.emit(ServerEvent.ConnectionReady, conn, id);
+            else {
+                this.emit( ServerEvent.ConnectionReady, conn, id );
             }
-        });
+        } );
 
-        conn.on('esl::end', () =>
-        {
-            this.emit(ServerEvent.ConnectionClose, conn, id);
+        conn.on( 'esl::end', () => {
+            this.emit( ServerEvent.ConnectionClose, conn, id );
             delete this.connections[id];
-        });
+        } );
     }
 }
--- test/fixtures/setup.ts
+++
@@ -1,7 +0,0 @@
-import * as sinonChai from 'sinon-chai';
-import { use } from 'chai';
-
-export function setupSinonChai()
-{
-    use(sinonChai);
-}
